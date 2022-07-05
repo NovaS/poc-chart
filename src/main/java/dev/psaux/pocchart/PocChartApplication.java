2fc39377520ab1249e13e955066d5a7e458ea44d
Nova Saputra <nova.saputra@gmail.com>
2022-07-06 00:33:48 +0700

    initial commit and setup

---
+++ src/main/java/dev/psaux/pocchart/PocChartApplication.java
@@ -0,0 +1,13 @@
+package dev.psaux.pocchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocChartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocChartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/psaux/pocchart/PocChartApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.psaux.pocchart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocChartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
